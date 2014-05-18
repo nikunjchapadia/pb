4b010fbf6fc5de211871f60bb662edf9b09bed8c
nikunjchapadia <nikunj.chapadia@gmail.com>
2014-05-18 14:37:15 -0700

    Added app nav bar

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -3,7 +3,7 @@
 angular.module('pbApp')
   .controller('NavbarCtrl', function ($scope, $location) {
     $scope.menu = [{
-      'title': 'Home',
+      'title': 'pb',
       'link': '/'
     }];
     
