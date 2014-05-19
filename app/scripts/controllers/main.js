dd3a436a2dd22cb9b49cf153209b40c8a40f487a
nikunjchapadia <nikunj.chapadia@gmail.com>
2014-05-18 17:46:58 -0700

    FB login

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,4 +5,30 @@ angular.module('pbApp')
         $http.get('/api/awesomeThings').success(function (awesomeThings) {
             $scope.awesomeThings = "";
         });
+
+        $scope.getUserInfo = function () {
+            $scope.userId = undefined;
+            $scope.accessToken = undefined;
+            FB.getLoginStatus(function(response) {
+                if (response.status === 'connected') {
+                    // the user is logged in and has authenticated your
+                    // app, and response.authResponse supplies
+                    // the user's ID, a valid access token, a signed
+                    // request, and the time the access token
+                    // and signed request each expire
+                    $scope.userId = response.authResponse.userID;
+                    $scope.accessToken = response.authResponse.accessToken;
+                    $scope.expiresIn = response.authResponse.expiresIn;
+                    console.log($scope.userId);
+                    console.log($scope.accessToken);
+                } else if (response.status === 'not_authorized') {
+                    // the user is logged in to Facebook,
+                    // but has not authenticated your app
+                } else {
+                    // the user isn't logged in to Facebook.
+                }
+            });
+        };
+
+
     });
--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -13,26 +13,4 @@ angular.module('pbApp')
             return route === $location.path();
         };
 
-        $scope.getUserInfo = function () {
-            $scope.userId = undefined;
-            $scope.accessToken = undefined;
-            FB.getLoginStatus(function(response) {
-                if (response.status === 'connected') {
-                    // the user is logged in and has authenticated your
-                    // app, and response.authResponse supplies
-                    // the user's ID, a valid access token, a signed
-                    // request, and the time the access token
-                    // and signed request each expire
-                    $scope.userId = response.authResponse.userID;
-                    $scope.accessToken = response.authResponse.accessToken;
-                    console.log($scope.userId);
-                    console.log($scope.accessToken);
-                } else if (response.status === 'not_authorized') {
-                    // the user is logged in to Facebook,
-                    // but has not authenticated your app
-                } else {
-                    // the user isn't logged in to Facebook.
-                }
-            });
-        };
     });
