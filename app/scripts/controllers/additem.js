d41183962bb18ae569a6315734112928e1fdf9f9
nikunjchapadia <nikunj.chapadia@gmail.com>
2014-05-19 16:02:17 -0700

    create new order screen

--- app/scripts/controllers/additem.js
+++ app/scripts/controllers/additem.js
@@ -2,5 +2,18 @@
 
 angular.module('pbApp').
     controller('AddItemCtrl', function ($scope, $http) {
+//        $scope.item = { name : undefined, brandName : undefined,
+//                        itemType: undefined, color: undefined,
+//                        realPrice : undefined,price : undefined
+//                       };
         $scope.message = "Add New Item";
+        $scope.item = $scope.item = { name : 'Bamboo Shoes', brandName : 'Bamboo',
+            itemType: 'shoe', color: 'mint',
+            realPrice : 50.00,price : 27.00
+        };
+
+        $scope.saveOrders = function () {
+            console.log($scope.item);
+            // take this one and save into db
+        };
     });
