39b2c276112cc72360525fd74aee4d3337699716
nikunjchapadia <nikunj.chapadia@gmail.com>
2014-05-19 21:39:06 -0700

    more fun with UI

--- app/scripts/controllers/additem.js
+++ app/scripts/controllers/additem.js
@@ -20,7 +20,8 @@ angular.module('pbApp').
         };
 
         $scope.getImages = function () {
-            var url = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=033e2f618063ad5e37d3f5bafd8b5c21&photoset_id=72157634587444986&extras=+license%2C+date_upload%2C+date_taken%2C+owner_name%2C+icon_server%2C+original_format%2C+last_update%2C+geo%2C+tags%2C+machine_tags%2C+o_dims%2C+views%2C+media%2C+path_alias%2C+url_sq%2C+url_t%2C+url_s%2C+url_m%2C+url_o&format=json&nojsoncallback=1&auth_token=72157644700009026-6e922b84bb102cbd&api_sig=ff93587d96ca9f2e2d35386cb4e17431";
+            //var url = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=033e2f618063ad5e37d3f5bafd8b5c21&photoset_id=72157634587444986&extras=+license%2C+date_upload%2C+date_taken%2C+owner_name%2C+icon_server%2C+original_format%2C+last_update%2C+geo%2C+tags%2C+machine_tags%2C+o_dims%2C+views%2C+media%2C+path_alias%2C+url_sq%2C+url_t%2C+url_s%2C+url_m%2C+url_o&format=json&nojsoncallback=1&auth_token=72157644700009026-6e922b84bb102cbd&api_sig=ff93587d96ca9f2e2d35386cb4e17431";
+            var url = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=033e2f618063ad5e37d3f5bafd8b5c21&photoset_id=72157644744193312&extras=+license%2C+date_upload%2C+date_taken%2C+owner_name%2C+icon_server%2C+original_format%2C+last_update%2C+geo%2C+tags%2C+machine_tags%2C+o_dims%2C+views%2C+media%2C+path_alias%2C+url_sq%2C+url_t%2C+url_s%2C+url_m%2C+url_o&format=json&nojsoncallback=1&auth_token=72157644700009026-6e922b84bb102cbd&api_sig=3da7bfab5b021e890f8f24040d5c21dd";
             $http.get(url).
                 success(function(data){
                     $scope.images = [];
