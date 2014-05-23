424df1eef0c04a9dc4f2dfd73dc1f015757107e5
nikunjchapadia <nikunj.chapadia@gmail.com>
2014-05-22 17:20:25 -0700

    image upload

--- app/scripts/controllers/additem.js
+++ app/scripts/controllers/additem.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('pbApp').
-    controller('AddItemCtrl', ['$scope', '$http','$upload', function ($scope, $http, $upload) {
+    controller('AddItemCtrl', ['$scope', '$http','$upload','$rootScope', function ($scope, $http, $upload,$rootScope) {
 //        $scope.item = { name : undefined, brandName : undefined,
 //                        itemType: undefined, color: undefined,
 //                        realPrice : undefined,price : undefined
@@ -35,19 +35,101 @@ angular.module('pbApp').
         };
 
         $scope.onFileSelect = function ($files) {
+            $scope.imageUploads = [];
             _.each($files, function (file) {
                 console.log(file);
-//                $scope.upload = $upload.upload({
-//                    url : '',
-//                    file : file
-//                }).
-//                progress(function(e){
-//                    console.log('% : ' + parseInt(100.0 * e.loaded / e.total));
-//                }).
-//                success(function(data,status,headrs,config){
-//                    console.log(data);
-//                });
+                $http.get('/api/s3Policy?mimeType='+ file.type).success(function(response) {
+                    var s3Params = response;
+                    $upload.upload({
+                        url: 'https://pbcreative.s3.amazonaws.com/',
+                        method: 'POST',
+                        data: {
+                            'key' : 's3UploadExample/'+ Math.round(Math.random()*10000) + '$$' + file.name,
+                            'acl' : 'public-read',
+                            'Content-Type' : file.type,
+                            'AWSAccessKeyId': s3Params.AWSAccessKeyId,
+                            'success_action_status' : '201',
+                            'Policy' : s3Params.s3Policy,
+                            'Signature' : s3Params.s3Signature
+                        },
+                        file: file
+                    }).then(function(response) {
+                        file.progress = parseInt(100);
+                        if (response.status === 201) {
+//                            var data = xml2json.parser(response.data),
+//                                parsedData;
+//                            parsedData = {
+//                                location: data.postresponse.location,
+//                                bucket: data.postresponse.bucket,
+//                                key: data.postresponse.key,
+//                                etag: data.postresponse.etag
+//                            };
+//                            $scope.imageUploads.push(parsedData);
+
+                        } else {
+                            alert('Upload Failed');
+                        }
+                    }, null, function(evt) {
+                        file.progress =  parseInt(100.0 * evt.loaded / evt.total);
+                    });
+                });
             });
         };
-
     }]);
+
+
+//angular.module('s3UploadApp')
+//    .controller('MainCtrl', function ($scope, $http, $location, $upload, $rootScope) {
+//        $scope.imageUploads = [];
+//        $scope.abort = function(index) {
+//            $scope.upload[index].abort();
+//            $scope.upload[index] = null;
+//        };
+//
+//        $scope.onFileSelect = function ($files) {
+//            $scope.files = $files;
+//            $scope.upload = [];
+//            for (var i = 0; i < $files.length; i++) {
+//                var file = $files[i];
+//                file.progress = parseInt(0);
+//                (function (file, i) {
+//                    $http.get('/api/s3Policy?mimeType='+ file.type).success(function(response) {
+//                        var s3Params = response;
+//                        $scope.upload[i] = $upload.upload({
+//                            url: 'https://' + $rootScope.config.awsConfig.bucket + '.s3.amazonaws.com/',
+//                            method: 'POST',
+//                            data: {
+//                                'key' : 's3UploadExample/'+ Math.round(Math.random()*10000) + '$$' + file.name,
+//                                'acl' : 'public-read',
+//                                'Content-Type' : file.type,
+//                                'AWSAccessKeyId': s3Params.AWSAccessKeyId,
+//                                'success_action_status' : '201',
+//                                'Policy' : s3Params.s3Policy,
+//                                'Signature' : s3Params.s3Signature
+//                            },
+//                            file: file,
+//                        }).then(function(response) {
+//                            file.progress = parseInt(100);
+//                            if (response.status === 201) {
+//                                var data = xml2json.parser(response.data),
+//                                    parsedData;
+//                                parsedData = {
+//                                    location: data.postresponse.location,
+//                                    bucket: data.postresponse.bucket,
+//                                    key: data.postresponse.key,
+//                                    etag: data.postresponse.etag
+//                                };
+//                                $scope.imageUploads.push(parsedData);
+//
+//                            } else {
+//                                alert('Upload Failed');
+//                            }
+//                        }, null, function(evt) {
+//                            file.progress =  parseInt(100.0 * evt.loaded / evt.total);
+//                        });
+//                    });
+//                }(file, i));
+//            }
+//        };
+//    });
+//
